5bafecdb0c0106e2eec3769a393035d90d62f3cb
limdanqi <limdanqi01@gmail.com>
2023-10-01 03:14:38 +0800

    Add project descriptions

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,15 +9,15 @@ scrollUp.addEventListener("click", () => {
   });
 });
 
-// Nav hamburgerburger selections
+// // Nav hamburgerburger selections
 
-const burger = document.querySelector("#burger-menu");
-const ul = document.querySelector("nav ul");
-const nav = document.querySelector("nav");
+// const burger = document.querySelector("#burger-menu");
+// const ul = document.querySelector("nav ul");
+// const nav = document.querySelector("nav");
 
-burger.addEventListener("click", () => {
-    ul.classList.toggle("show");
-  });
+// burger.addEventListener("click", () => {
+//     ul.classList.toggle("show");
+//   });
 
 // Close hamburger menu when a link is clicked
 
@@ -29,35 +29,6 @@ navLink.forEach((link) =>
     ul.classList.remove("show");
   })
 );
-
-// let slideIndex = 1;
-// showSlides(slideIndex);
-
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
 
 let slideIndex = [1,1,1,1];
 /* Class the members of each slideshow group with different CSS classes */
